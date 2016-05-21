657214169c12907e28a8f707ee70c5605f15a558
xiaoqianxin33 <996584895@qq.com>
2016-05-21 13:02:53 +0800

    模拟智慧北京

---
+++ RefreshListView_library/gen/com/itheima62/refreshlistview_library/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.itheima62.refreshlistview_library;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SmartBJ/gen/com/itheima62/smartbj/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.itheima62.smartbj;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
